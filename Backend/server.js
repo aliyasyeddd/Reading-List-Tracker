f8332c97d6b8cfdfe2b919114c965d6cdf8a9fb5
aliya mahaboob <122204472+aliyasyeddd@users.noreply.github.com>
2026-05-21 15:46:31 +0530

    creating express server , connecting to database

---
+++ Backend/server.js
@@ -0,0 +1,25 @@
+require("dotenv").config();
+const express = require("express")
+const connectDB = require("./config/db");
+
+const app = express()
+
+
+const port = process.env.PORT || 3000
+
+//parse JSON request bodies
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send('welcome to express.js');
+});
+
+connectDB().then(() => {
+    console.log("Database Connection established.... successfully");
+
+    app.listen(process.env.PORT || 3000, () => {
+        console.log(`API is running on port ${port}`);
+    });
+}).catch((err) => {
+    console.error("Failed to connect to DB:", err.message);
+});
